323baed6bad795905e9420ca3956f38c9fcc5efc
Thurston Stone <tstone2077@gmail.com>
2013-10-07 19:44:29 +0000

    Adding dynamic color for installation

--- install.sh
+++ install.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 #install startScreen and prompt for the current user
 
+
+if [ -z $1 ]; then
+  USERCOLOR="txtGreen"
+else
+  if [ "$1" == "-u" ]; then
+    echo Usage:
+    echo "  $0 [-u] : display usage"
+    echo "  -- or --"
+    echo "  $0 [userColor] [hostColor] [workingDirColor]: if a color is present, it sets the respective color as such"
+    echo "  available colors:"
+    for color in $(grep ".*=.*# " prompt  | sed 's/=.*//'); do
+        if [ "$color" != "reset" ]; then
+           echo -n $color,
+        fi
+    done;
+    echo ""
+
+  else
+     USERCOLOR=$1
+  fi
+fi
+if [ -z $2 ]; then
+  HOSTCOLOR="undGreen"
+else
+  HOSTCOLOR=$2
+fi
+if [ -z $3 ]; then
+  WDCOLOR="txtCyan"
+else
+  WDCOLOR=$3
+fi
+
 USERNAME=$(basename $HOME)
 SCRIPT_DIR=$(dirname $0)
 ROOT_DIR=~/.$USERNAME
@@ -33,6 +65,11 @@ function install_file
 }
 
 install_file $ROOT_DIR $SCRIPT_DIR prompt .bashrc
+#edit the prompt file
+sed -i 's/userColor=.*/userColor=$USERCOLOR/' $ROOT_DIR/prompt
+sed -i 's/hostColor=.*/hostColor=$HOSTCOLOR/' $ROOT_DIR/prompt
+sed -i 's/wdColor=.*/wdColor=$WDCOLOR/' $ROOT_DIR/prompt
+
 install_file $ROOT_DIR $SCRIPT_DIR startTmux .bashrc
 #install tmux configuration file
 cp $CURDIR/tmux.conf $HOME/.tmux.conf 2> /dev/null
